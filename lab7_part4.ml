dafe99b4ce3e71393f636a6f39f41642e081d5a7
Emma Rogge <emmarogge@gmail.com>
2019-03-10 22:46:50 -0400

    Figured out how to unit-test exceptions; poly_abstract_stack_module passing tests.

--- lab7_part4.ml
+++ lab7_part4.ml
@@ -18,91 +18,92 @@ files to complete all exercises:
  *)
 
 (*======================================================================
-Part 4: Polymorphic abstract types
+  Part 4: Polymorphic abstract types
 
-You may have noticed that the stack module in Part 3 focused
-exclusively on int types. But this doesn't have to be so: we can also
-create modules with polymorphic abstract data types, even ones
-protected by a signature.
+  You may have noticed that the stack module in Part 3 focused
+  exclusively on int types. But this doesn't have to be so: we can also
+  create modules with polymorphic abstract data types, even ones
+  protected by a signature.
 
-Below is a signature for a stack data structure, but providing a
-polymorphic abstract type so that we can generalize stacks to be int
-stacks, string stacks, stacks of all sorts. *)
+  Below is a signature for a stack data structure, but providing a
+  polymorphic abstract type so that we can generalize stacks to be int
+  stacks, string stacks, stacks of all sorts. *)
 
 module type STACK =
-  sig
-    exception EmptyStack
-    type 'a stack
-    val empty : 'a stack
-    val push : 'a -> 'a stack -> 'a stack
-    val top : 'a stack -> 'a
-    val pop : 'a stack -> 'a stack
-  end ;;
+sig
+  exception EmptyStack
+  type 'a stack
+  val empty : 'a stack
+  val push : 'a -> 'a stack -> 'a stack
+  val top : 'a stack -> 'a
+  val pop : 'a stack -> 'a stack
+end ;;
 
 (*......................................................................
-Exercise 4A: Define the stack module implementation. First, decide how
-you'll represent the stack, then implement each of the functions in
-the signature based on your decision. You may want to look at part 3
-for inspiration, but this implementation may differ from your previous
-implementation based on a design choice we describe below.
+  Exercise 4A: Define the stack module implementation. First, decide how
+  you'll represent the stack, then implement each of the functions in
+  the signature based on your decision. You may want to look at part 3
+  for inspiration, but this implementation may differ from your previous
+  implementation based on a design choice we describe below.
 
-We helped you out a little and defined "top" and "pop" for you,
-below. These rely on a helper function called "pop_helper", which
-is one you must implement. It should accept a stack and return a tuple
-containing the first element and the stack with the first element
-removed.
+  We helped you out a little and defined "top" and "pop" for you,
+  below. These rely on a helper function called "pop_helper", which
+  is one you must implement. It should accept a stack and return a tuple
+  containing the first element and the stack with the first element
+  removed.
 
-Notice that the pop_helper function does *not* appear in the
-signature, and will therefore not be accessible to functions outside
-of the module.
+  Notice that the pop_helper function does *not* appear in the
+  signature, and will therefore not be accessible to functions outside
+  of the module.
 
-You'll want to take advantage of the EmptyStack exception provided in
-the module; raise it if an attempt is made to examine or pop the
-top of an empty stack.
-......................................................................*)
+  You'll want to take advantage of the EmptyStack exception provided in
+  the module; raise it if an attempt is made to examine or pop the
+  top of an empty stack.
+  ......................................................................*)
 
 module Stack : STACK =
-  struct
-    exception EmptyStack
+struct
+  exception EmptyStack
 
-    type 'a stack = 'a list
+  type 'a stack = 'a list
 
-    (* empty -- An empty stack *)
-    let empty : 'a stack = []
+  (* empty -- An empty stack *)
+  let empty : 'a stack = []
 
-    (* push i s -- Adds an element i to the top of stack s *)
-    let push (i : 'a) (s : 'a stack) : 'a stack = i :: s 
+  (* push i s -- Adds an element i to the top of stack s *)
+  let push (i : 'a) (s : 'a stack) : 'a stack = i :: s
 
-    (* pop_helper s -- Returns a pair of the front element of the
-       stack and a stack containing the remaining elements *)
-    let pop_helper (s : 'a stack) : ('a * 'a stack) =
-      match s with
-      | [] -> raise EmptyStack
-      | hd::tl -> (hd, tl) ;;
+  (* pop_helper s -- Returns a pair of the front element of the
+     stack and a stack containing the remaining elements *)
+  let pop_helper (s : 'a stack) : ('a * 'a stack) =
+    match s with
+    | [] -> raise EmptyStack
+    | hd::tl -> (hd, tl)
 
-    let top (s: 'a stack) : 'a =
-      fst (pop_helper s)
-          
-    let pop (s : 'a stack) : 'a stack =
-      snd (pop_helper s)
-  end ;;
+  let top (s: 'a stack) : 'a =
+    fst (pop_helper s)
+
+  let pop (s : 'a stack) : 'a stack =
+    snd (pop_helper s)
+end ;;
 
 (*......................................................................
-Exercise 4B: Write a function, s, that takes a unit argument and uses
-your Stack module to return a new stack with the following strings
-pushed in order: "Computer", "Science", "51".
-......................................................................*)
+  Exercise 4B: Write a function, s, that takes a unit argument and uses
+  your Stack module to return a new stack with the following strings
+  pushed in order: "Computer", "Science", "51".
+  ......................................................................*)
 
-let s = let open Stack in
+let s () =
+  let open Stack in
   empty
   |> push "Computer"
   |> push "Science"
-  |> push "51" ;; ;;
+  |> push "51" ;;
 
 (*......................................................................
-Exercise 4C: Write an expression to generate a stack with the s
-function, above, and pull the top element from it, naming the result
-top_el.
-......................................................................*)
+  Exercise 4C: Write an expression to generate a stack with the s
+  function, above, and pull the top element from it, naming the result
+  top_el.
+  ......................................................................*)
 
 let top_el : string = Stack.top (s ()) ;;
--- lab7_tests.ml
+++ lab7_tests.ml
@@ -2,76 +2,79 @@ open CS51 ;;
 open Lab7_part1;;
 (*Part I Unit Tests*)
 open Math;;
-let test_math_module () = 
-	let title = "PART 1" in
-	Printf.printf "========\n";
-	Printf.printf "%s \n" title;
-	Printf.printf "========\n";
-	unit_test((pi) = 3.141592) "pi";
-	unit_test((cos 90.0) = ~-.0.448073616129170127) "cos float";
-	unit_test((sin 90.0) = 0.89399666360055785) "sin float";
-	unit_test((sum 10.0 13.0) = 23.0) "sum float";
-	unit_test((max [10.0; ~-.1.0; 7.3; ~-.100.9;]) = Some 10.0) "max floats";
-	unit_test(result = Some 6.53589793076241867e-07) "result floats";
-	unit_test(result_local_open = Some 6.53589793076241867e-07) "result_local_open floats";;
+let test_math_module () =
+  	let title = "PART 1" in
+  	Printf.printf "========\n";
+  	Printf.printf "%s \n" title;
+  	Printf.printf "========\n";
+  	unit_test((pi) = 3.141592) "pi";
+  	unit_test((cos 90.0) = ~-.0.448073616129170127) "cos float";
+  	unit_test((sin 90.0) = 0.89399666360055785) "sin float";
+  	unit_test((sum 10.0 13.0) = 23.0) "sum float";
+  	unit_test((max [10.0; ~-.1.0; 7.3; ~-.100.9;]) = Some 10.0) "max floats";
+  	unit_test(result = Some 6.53589793076241867e-07) "result floats";
+  	unit_test(result_local_open = Some 6.53589793076241867e-07) "result_local_open floats";;
 
 (*Part II Unit Tests*)
 open Color;;
 let test_color_module () =
-	let title = "PART 2" in
-	Printf.printf "========\n";
-	Printf.printf "%s \n" title;
-	Printf.printf "========\n";
-	let c = color_named Red in
-	unit_test((to_color 255 0 0) = c) "to_color";
-	unit_test((red c) = 255) "red";
-	unit_test((blue c) = 0) "blue";
-	unit_test((green c) = 0) "green";
-	unit_test((color_named Green) = to_color 0 255 0) "color_named" ;;
+  	let title = "PART 2" in
+  	Printf.printf "========\n";
+  	Printf.printf "%s \n" title;
+  	Printf.printf "========\n";
+  	let c = color_named Red in
+  	unit_test((to_color 255 0 0) = c) "to_color";
+  	unit_test((red c) = 255) "red";
+  	unit_test((blue c) = 0) "blue";
+  	unit_test((green c) = 0) "green";
+  	unit_test((color_named Green) = to_color 0 255 0) "color_named" ;;
 
 (*Part III Unit Tests*)
 open Lab7_part3 ;;
 let test_int_list_stack_module () =
-	let title = "PART 3" in
-	Printf.printf "========\n";
-	Printf.printf "%s \n" title;
-	Printf.printf "========\n";
-	let open IntListStack in
-	let s = [] in
-	unit_test((empty) = s) "empty";
-	unit_test((push 7 s) = [7]) "push first" ;
-	let s = [7] in
-	unit_test((push 3 s) = [3;7]) "push next" ;
-	let s = [7;4;5] in
-	unit_test((top s) = 7) "top";
-	unit_test((pop s) = [4;5]) "pop";
-	unit_test(small_stack () = [1;5]) "small_stack";
-	unit_test(last_el = 1) "last_el";
-	unit_test(invert_stack (small_stack ()) = [5;1]) "invert_stack";
-	unit_test(bad_el = 5) "bad_el";
-	let s = (SafeIntListStack.push 1 (SafeIntListStack.push 5 (SafeIntListStack.empty))) in 
-	unit_test((safe_stack ()) = s) "safe_stack";;
+  	let title = "PART 3" in
+  	Printf.printf "========\n";
+  	Printf.printf "%s \n" title;
+  	Printf.printf "========\n";
+  	let open IntListStack in
+  	let s = [] in
+  	unit_test((empty) = s) "empty";
+  	unit_test((push 7 s) = [7]) "push first" ;
+  	let s = [7] in
+  	unit_test((push 3 s) = [3;7]) "push next" ;
+  	let s = [7;4;5] in
+  	unit_test((top s) = 7) "top";
+  	unit_test((pop s) = [4;5]) "pop";
+  	unit_test(small_stack () = [1;5]) "small_stack";
+  	unit_test(last_el = 1) "last_el";
+  	unit_test(invert_stack (small_stack ()) = [5;1]) "invert_stack";
+  	unit_test(bad_el = 5) "bad_el";
+  	let s = (SafeIntListStack.push 1 (SafeIntListStack.push 5 (SafeIntListStack.empty))) in
+  	unit_test((safe_stack ()) = s) "safe_stack";;
+
 
 
 (*Part IV Unit Tests*)
-(* open Lab7_part4 ;;
 let test_poly_abstract_stack_module () =
-	let title = "PART 4" in
-	Printf.printf "========\n";
-	Printf.printf "%s \n" title;
-	Printf.printf "========\n";
-	let open Stack in
-	let s = empty in
-	let s' = push 7 s in
-	unit_test(push 7 s = s');; *)
+  	let open Lab7_part4 in
+  	let title = "PART 4" in
+  	Printf.printf "========\n";
+  	Printf.printf "%s \n" title;
+  	Printf.printf "========\n";
+  	let open Stack in
+  	let s = empty in
+  	(* let s' = push 7 s in *)
+  	unit_test
+    (try (pop s); false with EmptyStack -> true) "empty" ;;
 
 
 
 
-let test_all () = 
-	test_math_module () ;
-	test_color_module () ;
-	test_int_list_stack_module ();;
-	(* test_poly_abstract_stack_module () ;; *)
+
+let test_all () =
+  	test_math_module () ;
+  	test_color_module () ;
+  	test_int_list_stack_module ();
+  	test_poly_abstract_stack_module () ;;
 
 let _ = test_all ();;
